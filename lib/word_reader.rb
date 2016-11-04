70aae31faadf23bc755d46260cc08cdc69a7555c
Ekaterina Ionidi <cat81.06@gmail.com>
2016-11-04 11:28:35 +0300

    Update word_reader.rb

--- lib/word_reader.rb
+++ lib/word_reader.rb
@@ -1,18 +1,14 @@
 # encoding: utf-8
 
 # Класс, отвечающий за чтение слова для игры
-
 class WordReader
-  attr_reader :ARGV[0]
 
   def file_not_exist?(file_name)
     !File.exist?(file_name)
   end
 
   def read_from_file(file_name)
-    if file_not_exist?(file_name)
-      return nil
-    end
+    nil if file_not_exist?(file_name)
 
     f = File.new(file_name, "r:UTF-8")
     lines = f.readlines
